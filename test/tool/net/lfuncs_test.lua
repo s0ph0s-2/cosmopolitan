s0ph0s-2/cosmopolitan

commit 2c9bc443f21181d18ad0b35b9cc3702b22faf219

Implement raw system call for redbean lua code
You can now call functions like fork() from Lua and it'll work across
all supported platforms, including Windows. This gives you a level of
control of the system that Lua traditionally hasn't been able to have
due to its focus on old portable stdio rather modern POSIX APIs. Demo
code has been added to redbean-demo.com to show how it works.

This change also modifies Lua so that integer literals with a leading
zero will be interpreted as octal. That should help avoid shooting in
the foot with POSIX APIs that frequently use octal mode bits.

This change fixes a bug in opendir(".") on New Technology.

Lastly, redbean will now serve crash reports to private network IPs.
This is consistent with other frameworks. However that isn't served
to public IPs unless the -E flag is passed to redbean at startup.

third_party/lua/lobject.c
@@ -7,6 +7,7 @@
 #define lobject_c
 #define LUA_CORE
 
+#include "libc/intrin/kprintf.h"
 #include "third_party/lua/lctype.h"
 #include "third_party/lua/ldebug.h"
 #include "third_party/lua/ldo.h"
@@ -242,7 +243,9 @@ static const char *l_str2d (const char *s, lua_Number *result) {
 
 
 #define MAXBY10		cast(lua_Unsigned, LUA_MAXINTEGER / 10)
+#define MAXBY8		cast(lua_Unsigned, LUA_MAXINTEGER / 8)
 #define MAXLASTD	cast_int(LUA_MAXINTEGER % 10)
+#define MAXLASTD8	cast_int(LUA_MAXINTEGER % 8)
 
 static const char *l_str2int (const char *s, lua_Integer *result) {
   lua_Unsigned a = 0;
@@ -255,6 +258,15 @@ static const char *l_str2int (const char *s, lua_Integer *result) {
     s += 2;  /* skip '0x' */
     for (; lisxdigit(cast_uchar(*s)); s++) {
       a = a * 16 + luaO_hexavalue(*s);
+      empty = 0;
+    }
+  }
+  else if (s[0] == '0') {  /* [jart] octal is the best radix */
+    for (s += 1; lisdigit(cast_uchar(*s)); s++) {
+      int d = *s - '0';
+      if (a >= MAXBY8 && (a > MAXBY8 || d > MAXLASTD8 + neg))  /* overflow? */
+        return NULL;  /* do not accept it (as integer) */
+      a = a * 8 + d;
       empty = 0;
     }
   }

libc/calls/calls.h
@@ -229,7 +229,7 @@ uint32_t getgid(void) nosideeffect;
 uint32_t getpgrp(void) nosideeffect;
 uint32_t getsid(int) nosideeffect;
 uint32_t getuid(void) nosideeffect;
-uint32_t umask(int32_t);
+uint32_t umask(uint32_t);
 void rewinddir(DIR *);
 void sync(void);
 int getloadavg(double *, int);

libc/calls/dup3.c
@@ -32,7 +32,7 @@
  * @param oldfd isn't closed afterwards
  * @param newfd if already assigned, is silently closed beforehand;
  *     unless it's equal to oldfd, in which case dup2() is a no-op
- * @flags can have O_CLOEXEC
+ * @param flags can have O_CLOEXEC
  * @see dup(), dup2()
  */
 int dup3(int oldfd, int newfd, int flags) {

libc/calls/getppid.c
@@ -17,19 +17,23 @@
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
 #include "libc/calls/internal.h"
+#include "libc/calls/strace.internal.h"
 
 /**
  * Returns parent process id.
  * @asyncsignalsafe
  */
 int getppid(void) {
+  int rc;
   if (!IsWindows()) {
     if (!IsNetbsd()) {
-      return sys_getppid();
+      rc = sys_getppid();
     } else {
-      return sys_getpid().dx;
+      rc = sys_getpid().dx;
     }
   } else {
-    return sys_getppid_nt();
+    rc = sys_getppid_nt();
   }
+  STRACE("getppid() → %d% m", rc);
+  return rc;
 }

libc/calls/getpriority.c
@@ -18,6 +18,7 @@
 ╚─────────────────────────────────────────────────────────────────────────────*/
 #include "libc/calls/calls.h"
 #include "libc/calls/internal.h"
+#include "libc/calls/strace.internal.h"
 
 /**
  * Returns nice value of thing.
@@ -28,9 +29,12 @@
  * @see setpriority(), nice()
  */
 int getpriority(int which, unsigned who) {
+  int rc;
   if (!IsWindows()) {
-    return sys_getpriority(which, who) - 20;
+    rc = sys_getpriority(which, who) - 20;
   } else {
-    return sys_getsetpriority_nt(which, who, 0, sys_getpriority_nt);
+    rc = sys_getsetpriority_nt(which, who, 0, sys_getpriority_nt);
   }
+  STRACE("getpriority(%d, %u) → %d% m", which, who, rc);
+  return rc;
 }

libc/calls/internal.h
@@ -220,6 +220,7 @@ u32 sys_getgid(void) hidden;
 u32 sys_getsid(int) hidden;
 u32 sys_gettid(void) hidden;
 u32 sys_getuid(void) hidden;
+u32 sys_umask(u32) hidden;
 void *__sys_mmap(void *, u64, u32, u32, i64, i64, i64) hidden;
 void *sys_mremap(void *, u64, u64, i32, void *) hidden;
 void sys_exit(int) hidden;

libc/calls/setegid.c
@@ -17,10 +17,14 @@
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
 #include "libc/calls/calls.h"
+#include "libc/calls/strace.internal.h"
 
 /**
  * Sets effective group ID.
  */
-int setegid(unsigned gid) {
-  return setregid(-1, gid);
+int setegid(unsigned egid) {
+  int rc;
+  rc = setregid(-1, egid);
+  STRACE("%s(%u) → %d% m", "setegid", egid, rc);
+  return rc;
 }

libc/calls/seteuid.c
@@ -17,10 +17,14 @@
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
 #include "libc/calls/calls.h"
+#include "libc/calls/strace.internal.h"
 
 /**
  * Sets effective user ID.
  */
 int seteuid(unsigned euid) {
-  return setreuid(-1, euid);
+  int rc;
+  rc = setreuid(-1, euid);
+  STRACE("%s(%u) → %d% m", "seteuid", euid, rc);
+  return rc;
 }

commit 064d855b335492fac503fd3a3f6aed420c1e3a27

Fix Lua type of zero
This change fixes a regression from 2c9bc443 which resulted in the
integer literal `0` being interpreted as a floating point number. This
should also fix a reported issue with Fennel integration.

third_party/lua/lobject.c
@@ -293,7 +293,7 @@ static const char *l_str2int (const char *s, lua_Integer *result) {
     }
   }
   else if (s[0] == '0') {  /* [jart] octal is the best radix */
-    for (s += 1; lisdigit(cast_uchar(*s)); s++) {
+    for (; lisdigit(cast_uchar(*s)); s++) {
       int d = *s - '0';
       if (a >= MAXBY8 && (a > MAXBY8 || d > MAXLASTD8 + neg))  /* overflow? */
         return NULL;  /* do not accept it (as integer) */

test/tool/net/lfuncs_test.lua
@@ -32,6 +32,16 @@ assert(Bsf(0x80000001) == 0)
 assert(Lemur64() == 0x1940efe9d47ae889)
 assert(Lemur64() == 0xd4b3103f567f9974)
 
+assert(EncodeLua(nil) == "nil")
+assert(EncodeLua(0) == "0")
+assert(EncodeLua(3.14) == "3.14")
+assert(EncodeLua({1, 2}) == "{1, 2}")
+
+assert(EncodeJson(nil) == "null")
+assert(EncodeJson(0) == "0")
+assert(EncodeJson(3.14) == "3.14")
+assert(EncodeJson({1, 2}) == "[1,2]")
+
 assert(hex(0x1940efe9d47ae889) == "0x1940efe9d47ae889")
 assert(oct(0x1940efe9d47ae889) == "0145007376472436564211")
 assert(bin(0x1940efe9d47ae889) == "0b0001100101000000111011111110100111010100011110101110100010001001")