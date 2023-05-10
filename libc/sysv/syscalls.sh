s0ph0s-2/cosmopolitan

commit 58df5b601fd9313f2f83e4a6a0fc7cca636323f7

Improve cancellations, randomness, and time
- Exhaustively document cancellation points
- Rename SIGCANCEL to SIGTHR just like BSDs
- Further improve POSIX thread cancellations
- Ensure asynchronous cancellations work correctly
- Elevate the quality of getrandom() and getentropy()
- Make futexes cancel correctly on OpenBSD 6.x and 7.x
- Add reboot.com and shutdown.com to examples directory
- Remove underscore prefix from awesome timespec_*() APIs
- Create assertions that help verify our cancellation points
- Remove bad timespec APIs (cmp generalizes eq/ne/gt/gte/lt/lte)

libc/sysv/syscalls.sh
@@ -100,7 +100,8 @@ scall	__sys_wait4		0x9c180b807280783d	globl hidden
 scall	sys_kill		0x02507a025202503e	globl hidden # kill(pid, sig, 1) b/c xnu
 scall	sys_killpg		0x092fff092fffffff	globl hidden
 scall	sys_clone		0x11fffffffffff038	globl hidden
-scall	sys_tkill		0x13e0771b121480c8	globl hidden # thr_kill() on freebsd; _lwp_kill() on netbsd; thrkill() on openbsd where arg3 should be 0; __pthread_kill() on XNU
+scall	sys_tkill		0x13e0771b121480c8	globl hidden # thr_kill() on FreeBSD; _lwp_kill() on NetBSD; thrkill() on OpenBSD where arg3 should be 0 or tcb; __pthread_kill() on XNU
+scall	sys_tgkill		0xffffff1e1ffff0ea	globl hidden # thr_kill2() on FreeBSD
 scall	sys_futex		0x0a60531c6ffff0ca	globl hidden # raises SIGSYS on NetBSD; _umtx_op() on FreeBSD
 scall	sys_futex_cp		0x8a68539c6ffff8ca	globl hidden # intended for futex wait ops
 scall	sys_set_robust_list	0x0a7ffffffffff111	globl # no wrapper
@@ -265,7 +266,6 @@ scall	sys_ktimer_settime	0xffffff0edfffffff	globl # no wrapper
 scall	sys_clock_settime	0x1ac0580e9ffff0e3	globl # no wrapper
 scall	sys_clock_gettime	0x1ab0570e8ffff0e4	globl hidden # Linux 2.6+ (c. 2003); XNU uses magic address
 scall	sys_clock_getres	0x1ad0590eaffff0e5	globl hidden
-scall	sys_tgkill		0xfffffffffffff0ea	globl hidden
 scall	sys_mbind		0xfffffffffffff0ed	globl # no wrapper; numa numa yeah
 scall	set_mempolicy		0xfffffffffffff0ee	globl
 scall	get_mempolicy		0xfffffffffffff0ef	globl
@@ -286,6 +286,7 @@ scall	sys_inotify_init	0xfffffffffffff0fd	globl # wicked # no wrapper
 scall	sys_inotify_add_watch	0xfffffffffffff0fe	globl # no wrapper
 scall	sys_inotify_rm_watch	0xfffffffffffff0ff	globl # no wrapper
 scall	__sys_openat		0x9d49419f329cf901	globl hidden # Linux 2.6.16+ (c. 2007)
+scall	__sys_openat_nc		0x1d41411f321d0101	globl hidden # openat_nocancel() on xnu
 scall	sys_mkdirat		0x1cd13e1f021db102	globl hidden
 scall	sys_fchownat		0x1d013b1eb21d4104	globl hidden # @asyncsignalsafe
 scall	sys_utime		0xfffffffffffff084	globl hidden
@@ -350,7 +351,7 @@ scall	sys_sched_setattr	0xfffffffffffff13a	globl #  ├─ desktop replaced with
 scall	sys_sched_getattr	0xfffffffffffff13b	globl #  ├─ karen sandler requires systemd init and boot for tablet gui
 scall	sys_renameat2		0xfffffffffffff13c	globl #  └─ debian founder ian murdock found strangled with vacuum cord
 #scall	seccomp			0xfffffffffffff13d	globl # wrapped manually
-scall	sys_getrandom		0xfff807a3329f493e	globl hidden  # Linux 3.17+ and getentropy() on XNU/OpenBSD, coming to NetBSD in 9.2
+scall	sys_getrandom		0x85b007a3321f493e	globl hidden  # Linux 3.17+; FreeBSD 12+; NetBSD v9.2+; getentropy() on XNU/OpenBSD
 scall	sys_memfd_create	0xfffffffffffff13f	globl hidden
 scall	sys_kexec_file_load	0xfffffffffffff140	globl # no wrapper
 scall	sys_bpf			0xfffffffffffff141	globl # no wrapper
@@ -358,7 +359,7 @@ scall	sys_execveat		0xfffffffffffff142	globl # no wrapper
 scall	sys_userfaultfd		0xfffffffffffff143	globl # no wrapper; Linux 4.3+ (c. 2015)
 scall	sys_membarrier		0xfffffffffffff144	globl # no wrapper; Linux 4.3+ (c. 2015)
 scall	sys_mlock2		0xfffffffffffff145	globl # no wrapper; Linux 4.5+ (c. 2016)
-scall	sys_copy_file_range	0xffffff239ffff146	globl hidden # Linux 4.5+ (c. 2016), FreeBSD 13+
+scall	sys_copy_file_range	0xffffffa39ffff946	globl hidden # Linux 4.5+ (c. 2016), FreeBSD 13+
 scall	sys_preadv2		0xfffffffffffff147	globl # no wrapper
 scall	sys_pwritev2		0xfffffffffffff148	globl # no wrapper
 scall	sys_pkey_mprotect	0xfffffffffffff149	globl # no wrapper

third_party/nsync/futex.c
@@ -28,6 +28,7 @@
 #include "libc/errno.h"
 #include "libc/intrin/atomic.h"
 #include "libc/intrin/describeflags.internal.h"
+#include "libc/intrin/kprintf.h"
 #include "libc/intrin/strace.internal.h"
 #include "libc/intrin/weaken.h"
 #include "libc/limits.h"
@@ -38,6 +39,7 @@
 #include "libc/sysv/consts/timer.h"
 #include "libc/sysv/errfuns.h"
 #include "libc/thread/freebsd.internal.h"
+#include "libc/thread/posixthread.internal.h"
 #include "libc/thread/thread.h"
 #include "libc/thread/tls.h"
 #include "third_party/nsync/atomic.h"
@@ -118,26 +120,26 @@ static int nsync_futex_polyfill_ (atomic_int *w, int expect, struct timespec *ti
 	int64_t nanos, maxnanos;
 	struct timespec ts, deadline;
 
-	ts = nsync_time_now ();
+	ts = timespec_real ();
 	if (!timeout) {
-		deadline = nsync_time_no_deadline;
+		deadline = timespec_max;
 	} else if (FUTEX_TIMEOUT_IS_ABSOLUTE) {
 		deadline = *timeout;
 	} else {
-		deadline = nsync_time_add (ts, *timeout);
+		deadline = timespec_add (ts, *timeout);
 	}
 
 	nanos = 100;
 	maxnanos = __SIG_POLLING_INTERVAL_MS * 1000L * 1000;
-	while (nsync_time_cmp (deadline, ts) > 0) {
-		ts = nsync_time_add (ts, _timespec_fromnanos (nanos));
-		if (nsync_time_cmp (ts, deadline) > 0) {
+	while (timespec_cmp (deadline, ts) > 0) {
+		ts = timespec_add (ts, timespec_fromnanos (nanos));
+		if (timespec_cmp (ts, deadline) > 0) {
 			ts = deadline;
 		}
 		if (atomic_load_explicit (w, memory_order_acquire) != expect) {
 			return 0;
 		}
-		if ((rc = nsync_time_sleep_until (ts))) {
+		if ((rc = timespec_sleep_until (ts))) {
 			return -rc;
 		}
 		if (nanos < maxnanos) {
@@ -159,22 +161,22 @@ static int nsync_futex_wait_win32_ (atomic_int *w, int expect, char pshare, stru
 	if (timeout) {
 		deadline = *timeout;
 	} else {
-		deadline = nsync_time_no_deadline;
+		deadline = timespec_max;
 	}
 
 	while (!(rc = _check_interrupts (false, 0))) {
-		now = nsync_time_now ();
-		if (nsync_time_cmp (now, deadline) > 0) {
+		now = timespec_real ();
+		if (timespec_cmp (now, deadline) > 0) {
 			rc = etimedout();
 			break;
 		}
-		remain = nsync_time_sub (deadline, now);
-		interval = _timespec_frommillis (__SIG_POLLING_INTERVAL_MS);
-		wait = nsync_time_cmp (remain, interval) > 0 ? interval : remain;
+		remain = timespec_sub (deadline, now);
+		interval = timespec_frommillis (__SIG_POLLING_INTERVAL_MS);
+		wait = timespec_cmp (remain, interval) > 0 ? interval : remain;
 		if (atomic_load_explicit (w, memory_order_acquire) != expect) {
 			break;
 		}
-		if (WaitOnAddress (w, &expect, sizeof(int), _timespec_tomillis (wait))) {
+		if (WaitOnAddress (w, &expect, sizeof(int), timespec_tomillis (wait))) {
 			break;
 		} else {
 			ASSERT (GetLastError () == ETIMEDOUT);
@@ -186,6 +188,7 @@ static int nsync_futex_wait_win32_ (atomic_int *w, int expect, char pshare, stru
 
 int nsync_futex_wait_ (atomic_int *w, int expect, char pshare, struct timespec *timeout) {
 	int e, rc, op, fop;
+	struct PosixThread *pt = 0;
 
 	if (atomic_load_explicit (w, memory_order_acquire) != expect) {
 		return -EAGAIN;
@@ -210,21 +213,31 @@ int nsync_futex_wait_ (atomic_int *w, int expect, char pshare, struct timespec *
 		} else if (IsFreebsd ()) {
 			rc = sys_umtx_timedwait_uint (w, expect, pshare, timeout);
 		} else {
-			rc = sys_futex_cp (w, op, expect, timeout, 0, FUTEX_WAIT_BITS_);
-			if (IsOpenbsd() && rc > 0) {
-				// OpenBSD futex() returns errors as
-				// positive numbers without setting the
-				// carry flag. It's an irregular miracle
-				// because OpenBSD returns ECANCELED if
-				// futex() is interrupted w/ SA_RESTART
-				// so we're able to tell it apart from
-				// PT_MASKED which causes the wrapper
-				// to put ECANCELED into errno.
-				if (rc == ECANCELED) {
-					rc = EINTR;
+			if (IsOpenbsd()) {
+				// OpenBSD 6.8 futex() returns errors as
+				// positive numbers, without setting CF.
+				// This irregularity is fixed in 7.2 but
+				// unfortunately OpenBSD futex() defines
+				// its own ECANCELED condition, and that
+				// overlaps with our system call wrapper
+				if ((pt = (struct PosixThread *)__get_tls()->tib_pthread)) {
+					pt->flags &= ~PT_OPENBSD_KLUDGE;
 				}
-				errno = rc;
-				rc = -1;
+			}
+			rc = sys_futex_cp (w, op, expect, timeout, 0, FUTEX_WAIT_BITS_);
+			if (IsOpenbsd()) {
+				// Handle the OpenBSD 6.x irregularity.
+				if (rc > 0) {
+					errno = rc;
+					rc = -1;
+				}
+				// Check if ECANCELED came from the kernel
+				// because a SA_RESTART signal handler was
+				// invoked, such as our SIGTHR callback.
+				if (rc == -1 && errno == ECANCELED &&
+				    pt && (~pt->flags & PT_OPENBSD_KLUDGE)) {
+					errno = EINTR;
+				}
 			}
 		}
 		if (rc == -1) {

commit af356e1957b049b746ad73fc934b8cffaaf0a102

Remove sys_getrandom() on NetBSD
This fixes an apparent regression caused by
58df5b601fd9313f2f83e4a6a0fc7cca636323f7 where getrandom() on NetBSD 9.2
doesn't appear to work; ktrace oddly reports:

    1446      1 .ape     CALL  #91 (unimplemented getdopt)
    1446      1 .ape     RET   #91 (unimplemented getdopt) -1 errno 78
    Function not implemented
    1446      1 .ape     PSIG  SIGSYS SIG_DFL: code=SI_NOINFO

libc/sysv/calls/sys_getrandom.S
@@ -1,2 +1,2 @@
 #include "libc/sysv/macros.internal.h"
-.scall sys_getrandom,0x85b007a3321f493e,0x116,globl,hidden
+.scall sys_getrandom,0xfff007a3321f493e,0x116,globl,hidden

libc/sysv/syscalls.sh
@@ -351,7 +351,7 @@ scall	sys_sched_setattr	0xfffffffffffff13a	0x112	globl #  ├─ desktop replace
 scall	sys_sched_getattr	0xfffffffffffff13b	0x113	globl #  ├─ karen sandler requires systemd init and boot for tablet gui
 scall	sys_renameat2		0xfffffffffffff13c	0x114	globl #  └─ debian founder ian murdock found strangled with vacuum cord
 #scall	seccomp			0xfffffffffffff13d	0x115	globl # wrapped manually
-scall	sys_getrandom		0x85b007a3321f493e	0x116	globl hidden  # Linux 3.17+; FreeBSD 12+; NetBSD v9.2+; getentropy() on XNU/OpenBSD
+scall	sys_getrandom		0xfff007a3321f493e	0x116	globl hidden  # Linux 3.17+; FreeBSD 12+; NetBSD wut; getentropy() on XNU/OpenBSD
 scall	sys_memfd_create	0xfffffffffffff13f	0x117	globl hidden
 scall	sys_kexec_file_load	0xfffffffffffff140	0xfff	globl # no wrapper
 scall	sys_bpf			0xfffffffffffff141	0x118	globl # no wrapper

third_party/nsync/futex.c
@@ -16,6 +16,7 @@
 │ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR             │
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
+#include "libc/sysv/consts/futex.h"
 #include "libc/assert.h"
 #include "libc/atomic.h"
 #include "libc/calls/calls.h"
@@ -36,7 +37,6 @@
 #include "libc/nt/runtime.h"
 #include "libc/nt/synchronization.h"
 #include "libc/sysv/consts/clock.h"
-#include "libc/sysv/consts/futex.h"
 #include "libc/sysv/consts/timer.h"
 #include "libc/sysv/errfuns.h"
 #include "libc/thread/freebsd.internal.h"