s0ph0s-2/cosmopolitan

commit c099ad96dce752ec327425a1c0f0b5482df80cf5

Make improvements
- Every unit test now passes on Apple Silicon. The final piece of this
  puzzle was porting our POSIX threads cancelation support, since that
  works differently on ARM64 XNU vs. AMD64. Our semaphore support on
  Apple Silicon is also superior now compared to AMD64, thanks to the
  grand central dispatch library which lets *NSYNC locks go faster.

- The Cosmopolitan runtime is now more stable, particularly on Windows.
  To do this, thread local storage is mandatory at all runtime levels,
  and the innermost packages of the C library is no longer being built
  using ASAN. TLS is being bootstrapped with a 128-byte TIB during the
  process startup phase, and then later on the runtime re-allocates it
  either statically or dynamically to support code using _Thread_local.
  fork() and execve() now do a better job cooperating with threads. We
  can now check how much stack memory is left in the process or thread
  when functions like kprintf() / execve() etc. call alloca(), so that
  ENOMEM can be raised, reduce a buffer size, or just print a warning.

- POSIX signal emulation is now implemented the same way kernels do it
  with pthread_kill() and raise(). Any thread can interrupt any other
  thread, regardless of what it's doing. If it's blocked on read/write
  then the killer thread will cancel its i/o operation so that EINTR can
  be returned in the mark thread immediately. If it's doing a tight CPU
  bound operation, then that's also interrupted by the signal delivery.
  Signal delivery works now by suspending a thread and pushing context
  data structures onto its stack, and redirecting its execution to a
  trampoline function, which calls SetThreadContext(GetCurrentThread())
  when it's done.

- We're now doing a better job managing locks and handles. On NetBSD we
  now close semaphore file descriptors in forked children. Semaphores on
  Windows can now be canceled immediately, which means mutexes/condition

libc/intrin/mman.greg.c
@@ -47,6 +47,13 @@
 #define INVERT(x) (BANE + PHYSICAL(x))
 #define NOPAGE    ((uint64_t)-1)
 
+#define ABS64(x)                                     \
+  ({                                                 \
+    int64_t vAddr;                                   \
+    __asm__("movabs\t%1,%0" : "=r"(vAddr) : "i"(x)); \
+    vAddr;                                           \
+  })
+
 struct ReclaimedPage {
   uint64_t next;
 };
@@ -252,37 +259,56 @@ textreal void __setup_mman(struct mman *mm, uint64_t *pml4t, uint64_t top) {
   __invert_memory(mm, pml4t);
 }
 
+static textreal uint64_t __map_phdr(struct mman *mm, uint64_t *pml4t,
+                                    uint64_t b, uint64_t m,
+                                    struct Elf64_Phdr *p) {
+  uint64_t i, f, v;
+  f = PAGE_RSRV | PAGE_U;
+  if (p->p_flags & PF_W)
+    f |= PAGE_V | PAGE_RW;
+  else if (p->p_flags & (PF_R | PF_X))
+    f |= PAGE_V;
+  if (!(p->p_flags & PF_X)) f |= PAGE_XD;
+  for (i = 0; i < p->p_memsz; i += 4096) {
+    if (i < p->p_filesz) {
+      v = b + p->p_offset + i;
+      m = MAX(m, v);
+    } else {
+      v = __clear_page(BANE + __new_page(mm));
+    }
+    *__get_virtual(mm, pml4t, p->p_vaddr + i, true) = (v & PAGE_TA) | f;
+    __ref_page(mm, pml4t, v & PAGE_TA);
+  }
+  return m;
+}
+
 /**
  * Maps APE-defined ELF program headers into memory and clears BSS.
  */
 textreal void __map_phdrs(struct mman *mm, uint64_t *pml4t, uint64_t b,
                           uint64_t top) {
-  uint64_t i, f, v, m;
+  uint64_t m;
   struct Elf64_Phdr *p;
   extern char ape_phdrs[] __attribute__((__weak__));
   extern char ape_phdrs_end[] __attribute__((__weak__));
+  extern char ape_stack_pf[] __attribute__((__weak__));
+  extern char ape_stack_offset[] __attribute__((__weak__));
+  extern char ape_stack_vaddr[] __attribute__((__weak__));
+  extern char ape_stack_filesz[] __attribute__((__weak__));
+  extern char ape_stack_memsz[] __attribute__((__weak__));
   __setup_mman(mm, pml4t, top);
   for (p = (struct Elf64_Phdr *)INVERT(ape_phdrs), m = 0;
        p < (struct Elf64_Phdr *)INVERT(ape_phdrs_end); ++p) {
-    if (p->p_type == PT_LOAD || p->p_type == PT_GNU_STACK) {
-      f = PAGE_RSRV | PAGE_U;
-      if (p->p_flags & PF_W)
-        f |= PAGE_V | PAGE_RW;
-      else if (p->p_flags & (PF_R | PF_X))
-        f |= PAGE_V;
-      if (!(p->p_flags & PF_X)) f |= PAGE_XD;
-      for (i = 0; i < p->p_memsz; i += 4096) {
-        if (i < p->p_filesz) {
-          v = b + p->p_offset + i;
-          m = MAX(m, v);
-        } else {
-          v = __clear_page(BANE + __new_page(mm));
-        }
-        *__get_virtual(mm, pml4t, p->p_vaddr + i, true) = (v & PAGE_TA) | f;
-        __ref_page(mm, pml4t, v & PAGE_TA);
-      }
-    }
+    m = __map_phdr(mm, pml4t, b, m, p);
   }
+  m = __map_phdr(mm, pml4t, b, m,
+                 &(struct Elf64_Phdr){
+                     .p_flags = (uintptr_t)ape_stack_pf,
+                     .p_offset = (uintptr_t)ape_stack_offset,
+                     .p_vaddr = ABS64(ape_stack_vaddr),
+                     .p_filesz = (uintptr_t)ape_stack_filesz,
+                     .p_memsz = (uintptr_t)ape_stack_memsz,
+                 });
   mm->pdp = MAX(mm->pdp, m);
 }
 

commit b7617f605c9c64b074db2b388f804d16f25f58fd

[metal] Fix regression causing early crash in __new_page( ) (#900)
There was a glitch in the refactoring of __map_phdrs( ) in
commit c099ad96dc, which caused it to try to map the PT_NOTE
program segment into virtual memory, which in turn caused the
memory page at BANE to be wrongly remapped to the start of
the program image (0x100000) rather than physical address 0.
This affected subsequent page allocation operations because
the `struct mman` was located at BANE + 0x0500.

Co-authored-by: tkchia <tkchia-cosmo@gmx.com>

libc/intrin/mman.greg.c
@@ -263,6 +263,7 @@ static textreal uint64_t __map_phdr(struct mman *mm, uint64_t *pml4t,
                                     uint64_t b, uint64_t m,
                                     struct Elf64_Phdr *p) {
   uint64_t i, f, v;
+  if (p->p_type != PT_LOAD) return m;
   f = PAGE_RSRV | PAGE_U;
   if (p->p_flags & PF_W)
     f |= PAGE_V | PAGE_RW;
@@ -303,6 +304,7 @@ textreal void __map_phdrs(struct mman *mm, uint64_t *pml4t, uint64_t b,
   }
   m = __map_phdr(mm, pml4t, b, m,
                  &(struct Elf64_Phdr){
+                     .p_type = PT_LOAD,
                      .p_flags = (uintptr_t)ape_stack_pf,
                      .p_offset = (uintptr_t)ape_stack_offset,
                      .p_vaddr = ABS64(ape_stack_vaddr),