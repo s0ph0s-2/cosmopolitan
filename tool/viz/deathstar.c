s0ph0s-2/cosmopolitan

commit e7091d1496a8df9d9c2a1f229bacb3400273b0b3

[metal] Make deathstar.com draw onto both VGA and serial consoles (#955)
In addition, deathstar.com can now be booted up via UEFI.

tool/viz/deathstar.c
@@ -15,6 +15,9 @@
 #define FRAMERATE 23.976
 #define WRITE(s)  write(1, s, strlen(s))
 
+__static_yoink("vga_console");
+__static_yoink("EfiMain");
+
 struct Sphere {
   double cx, cy, cz, r;
 };

tool/viz/viz.mk
@@ -41,6 +41,7 @@ TOOL_VIZ_DIRECTDEPS =				\
 	LIBC_THREAD				\
 	LIBC_TIME				\
 	LIBC_TINYMATH				\
+	LIBC_VGA				\
 	LIBC_X					\
 	NET_HTTP				\
 	THIRD_PARTY_COMPILER_RT			\

commit 877139a647144057297250862afddb28c67590db

Fix regression when building deathstar.com for aarch64 (#957)
It turns out my earlier commit e7091d1496 caused a build with
MODE=aarch64 to fail.  The commit changed deathstar.c to link
in code to support a VGA console, but this is not implemented
yet for AArch64.  Thanks to @ahgamut for spotting this issue.

tool/viz/deathstar.c
@@ -15,8 +15,10 @@
 #define FRAMERATE 23.976
 #define WRITE(s)  write(1, s, strlen(s))
 
+#ifdef __x86_64__
 __static_yoink("vga_console");
 __static_yoink("EfiMain");
+#endif
 
 struct Sphere {
   double cx, cy, cz, r;